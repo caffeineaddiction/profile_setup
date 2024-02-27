b5a170d5f83b670d795e07a6612b138d8416e99f
James Lord <caffeineaddiction@gmail.com>
2024-02-27 17:09:34 +0000

    added target_user

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 # ansible-core
 
-/usr/bin/ansible-pull -U ssh://git@github.com:caffeineaddiction/profile_setup.git setup.yml
+/usr/bin/ansible-pull -U ssh://git@github.com:caffeineaddiction/profile_setup.git -e "target_user=${SUDO_USER:-$(whoami)}" setup.yml
