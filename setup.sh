75e3c3160bd6b0deb093be31ab8c5e36c8bd4c40
James <jlord@dvtrading.co>
2024-03-14 08:56:18 -0500

    added proxy comment

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 
 #yum install -y ansible-core
 
+# sudo http_proxy=http://127.0.0.1:54321 https_proxy=http://127.0.0.1:54321 ansible-pull -U https://github.com/caffeineaddiction/profile_setup.git -C "master" -e "target_user=${SUDO_USER:-$(whoami)}" --skip-tags vault -i localhost, setup.yml
 ansible-pull -U https://github.com/caffeineaddiction/profile_setup.git -C "master" -e "target_user=${SUDO_USER:-$(whoami)}" --skip-tags vault -i localhost, setup.yml
